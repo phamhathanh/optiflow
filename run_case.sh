96d99420c785fb5f3a8c20d58db26005c927929b
ThanhPH <phamhathanh130394@gmail.com>
2017-05-12 23:10:04 +0700

    New scheme.

--- run_case.sh
+++ run_case.sh
@@ -3,38 +3,26 @@ export FF_VERBOSITY=0;
 
 case "$1" in
     1)
-	echo "Launching test-case 1"
-	FreeFem++ main.edp --config 1 --resu resu\case1 --binit 1 --btarget 1e1 --tau 1e-2 --l0 0 --gamma .01
-	;;
+		echo "Launching test-case 1"
+        FreeFem++ main.edp --config 1 --resu resu/case1 --binit 1 --btarget 1e1 --tau 1e-2 --l0 0 --gamma .1 --raffinit 2e-2 --navsto 0 
+		;;
     2)
-	echo "Launching test-case 2"
-	FreeFem++ main.edp --config 2 --resu resu\case2 --l0 0 --binit 1e-1 --btarget 1e1 --tau 1e-2 --gamma .01 --errc 5e-3
-	;;
-    3)
-	echo "Launching test-case 3"
-	FreeFem++ main.edp --config 3 --resu resu\case3 --binit 0 --btarget 0 --tau 1e-2 --l0 0 --raffinit 5e-2 --gamma .01 --jjmax 30
-	;;
-    4)
-	echo "Launching test-case 4"
-	FreeFem++ main.edp --config 4 --resu resu\case4 --binit 1e1 --btarget 1e2 --tau 3e-3 --l0 15 --optraff 1 --raffinit 3e-2 --kc -1 --errc 2e-2
-	;;
-    5)
-	echo "Launching test-case 5"
-	FreeFem++ main.edp --config 5 --resu resu\case5 --binit 1e2 --btarget 1e2 --cv .97 --tau 1e-2 --l0 0 --optraff 0 --raffinit 3e-2 --beta 0 --gamma 1e-2
-	;;
+		echo "Launching test-case 2"
+        FreeFem++ main.edp --config 2 --resu resu/case2 --l0 0 --binit 1e-1 --btarget 1e1 --tau 1e-2 --gamma .01 --errc 5e-3 --navsto 0
+		;;
     6)
-	echo "Launching test-case 6"
-	FreeFem++ main.edp --config 6 --resu resu\case6 --binit 10 --btarget 100 --tau .01 --l0 0 --raffinit .03 --gamma .01
-	;;
-    7)
-	echo "Launching test-case 7"
-	FreeFem++ main.edp --config 7 --resu resu\case7 --binit 100 --btarget 1000 --tau .01 --l0 0 --raffinit .03 --beta 0 --gamma .01
-	;;
+		echo "Launching test-case 6"
+		FreeFem++ main.edp --config 6 --resu resu/case6 --binit 1 --btarget 10 --tau .01 --l0 0 --raffinit .03 --gamma .01 --navsto 0
+		;;
     8)
-	echo "Launching test-case 8"
-	FreeFem++ main.edp --config 8 --resu resu\case8 --binit 10 --btarget 100 --tau .01 --l0 0 --raffinit .03 --gamma .01
-	;;
+		echo "Launching test-case 8"
+		FreeFem++ main.edp --config 8 --resu resu/case8 --binit 1 --btarget 10 --tau .01 --l0 0 --raffinit .03 --gamma .01
+		;;
+    9)
+		echo "Launching test-case 9"
+		FreeFem++ main.edp --config 9 --resu resu/case9 --binit 1 --btarget 10 --tau 3e-3 --l0 0 --optraff 1 --raffinit 3e-2 --kc -1 --errc 2e-2 --navsto 0
+		;;
     *)
-	echo "Usage : $0 {1|2|3|4|5|6|7|8}"
-	;;
+		echo "Usage : $0 {1|2|6|8|9}"
+		;;
 esac
